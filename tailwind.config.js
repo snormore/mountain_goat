18016a86563e69776194d57b40e41ad252f5841f
Axel <axelshaded@gmail.com>
2020-10-22 19:43:08 +0800

    Adding styling for post
    Adding styling for post

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,24 @@
 module.exports = {
   purge: [`_site/**/*.html`],
   theme: {
+    // Some useful comment
+    fontFamily: {
+      sans: ['proxima-nova', '"Proxima Nova"', 'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',],
+      serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+      mono: ['Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
+    },
     extend: {},
   },
   variants: {},
